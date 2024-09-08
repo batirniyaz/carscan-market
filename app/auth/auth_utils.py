bc89d3ca92df7b2a1ae2db26b45a4a9e62f94df2
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-08 17:47:51 +0500

    create and add auth_utils

---
+++ app/auth/auth_utils.py
@@ -0,0 +1,23 @@
+from passlib.context import CryptContext
+import datetime
+from jose import jwt
+
+from app.config import SECRET_KEY, ALGORITHM
+
+SECRET = SECRET_KEY
+ALGORITHM = ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET, algorithm=ALGORITHM)
+    return encoded_jwt
