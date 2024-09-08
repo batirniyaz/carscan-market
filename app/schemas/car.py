efeb9d759e2623ea20a1952658eadf79bccf6d8f
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-08 21:55:46 +0500

    delete unnecessary schemas

--- app/schemas/car.py
+++ app/schemas/car.py
@@ -4,19 +4,13 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 
-class CarBase(BaseModel):
+class CarResponse(BaseModel):
+    id: int = Field(..., description="The ID of the car")
     number: str = Field(..., description="The number of the car")
     date: str = Field(..., description="The date of the car")
     time: str = Field(..., description="The time of the car")
     image_url: Optional[str] = Field(None, description="The image URL of the car")
 
-
-class CarCreate(CarBase):
-    pass
-
-
-class CarResponse(CarBase):
-    id: int = Field(..., description="The ID of the car")
     created_at: datetime.datetime = Field(..., description="The time the car was created")
     updated_at: datetime.datetime = Field(..., description="The time the car was updated")
 
