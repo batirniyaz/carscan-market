a21bb6aac8e2c634ad667a5134af08cdbb50db12
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-08 21:53:04 +0500

    include exception numbers router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .car import router as car_router
+from .exception_nums import router as exception_nums_router
 
 router = APIRouter()
 
 router.include_router(car_router, prefix="/car", tags=["Car"])
+router.include_router(exception_nums_router, prefix="/exception-nums", tags=["Exception Numbers"])
