fe402a5bcce64bd16cef2d041c873a07050b99c1
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-08 17:49:16 +0500

    add new endpoint login_with_phone_number

--- app/auth/base_config.py
+++ app/auth/base_config.py
@@ -1,8 +1,13 @@
 from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import BearerTransport, JWTStrategy, AuthenticationBackend
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.auth.database import User
+from app.auth.schemas import PhoneNumberLoginRequest
+from app.auth.auth_utils import verify_password, create_access_token
+
+from app.auth.database import User, get_async_session
 from app.auth.manager import get_user_manager
 from app.auth.schemas import UserRead, UserCreate
 from app.config import SECRET_KEY
@@ -46,3 +51,22 @@ router.include_router(
 @router.get("/authenticated-route")
 async def authenticated_route(user: User = Depends(current_active_user)):
     return {"message": f"Hello {user.email}!"}
+
+
+@router.post("/auth/jwt/login/phone")
+async def login_with_phone_number(request: PhoneNumberLoginRequest, db: AsyncSession = Depends(get_async_session)):
+    stmt = select(User).filter_by(phone_number=request.phone_number)
+    result = await db.execute(stmt)
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect phone number or password")
+
+    try:
+        if not verify_password(request.password, user.hashed_password):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect phone number or password")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="hash could not be identified")
+
+    access_token = create_access_token(data={"sub": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
