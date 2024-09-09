74a1c7009ff937e8504fb1f7c290b2733af80e9f
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-09 16:17:05 +0500

    include router daily_report

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,9 +2,12 @@ from fastapi import APIRouter
 from .car import router as car_router
 from .exception_nums import router as exception_nums_router
 from .export_data import router as export_data_router
+from .daily_report import router as daily_report_router
 
 router = APIRouter()
 
 router.include_router(car_router, prefix="/car", tags=["Car"])
 router.include_router(exception_nums_router, prefix="/exception-nums", tags=["Exception Numbers"])
 router.include_router(export_data_router, prefix="/export-data", tags=["Export Data"])
+router.include_router(daily_report_router, prefix="/daily-report", tags=["Daily Report"])
+
