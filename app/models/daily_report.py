3d367fe45785015e0b196271492160506a26a132
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-09 16:12:42 +0500

    create DailyReport model

---
+++ app/models/daily_report.py
@@ -0,0 +1,22 @@
+import datetime
+
+from sqlalchemy import JSON, TIMESTAMP
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.auth.database import Base
+
+
+class DailyReport(Base):
+    __tablename__ = 'daily_report'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    date: Mapped[str] = mapped_column()
+    top10: Mapped[list[dict]] = mapped_column(JSON, default=[])
+    general_count: Mapped[int] = mapped_column(default=0)
+    overall_count: Mapped[int] = mapped_column(default=0)
+
+    created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc))
+    updated_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc),
+                                                          onupdate=lambda: datetime.datetime.now(datetime.timezone.utc))
