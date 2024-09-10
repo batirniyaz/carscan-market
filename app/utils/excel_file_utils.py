305cd56d77f137920ac89791a6407fb10e17554d
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-10 21:18:33 +0500

    update create excel file

--- app/utils/excel_file_utils.py
+++ app/utils/excel_file_utils.py
@@ -1,7 +1,14 @@
+import os
+
 import pandas as pd
 
 
-def create_excel_file(data, file_name):
-    df = pd.DataFrame(data['graphic'])
-    df.to_excel(f"{file_name}.xlsx", index=False)
-    return f"{file_name}.xlsx"
+async def create_excel_file(data, file_name: str):
+    df = pd.DataFrame(data)
+    reports_dir = "app/reports"
+    if not os.path.exists(reports_dir):
+        os.makedirs(reports_dir, exist_ok=True)
+
+    excel_file_path = os.path.join(reports_dir, f"{file_name}.xlsx")
+    df.to_excel(excel_file_path, index=False)
+    return excel_file_path
