0c55c1de487be1a47e32f452f3830d46be9aa90e
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-10 19:03:52 +0500

    create unknown_car pydantic model

---
+++ app/schemas/unknown_car.py
@@ -0,0 +1,30 @@
+import datetime
+
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class UnknownCarResponse(BaseModel):
+    id: int = Field(..., description="The ID of the car")
+    number: str = Field(..., description="The number of the car")
+    date: str = Field(..., description="The date of the car")
+    time: str = Field(..., description="The time of the car")
+    image_url: Optional[str] = Field(None, description="The image URL of the car")
+
+    created_at: datetime.datetime = Field(..., description="The time the car was created")
+    updated_at: datetime.datetime = Field(..., description="The time the car was updated")
+
+    class Config:
+        from_attributes = True
+        validate_assignment = True
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "number": "unknown",
+                "date": "2022-01-01",
+                "time": "12:00",
+                "image_url": "http://example.com/image.jpg",
+                "created_at": "2022-01-01T12:00:00",
+                "updated_at": "2022-01-01T12:00:00"
+            }
+        }
