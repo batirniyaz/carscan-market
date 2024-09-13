e6d817236041143452fdcdcc4949dc9cf5efc358
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-13 18:36:05 +0500

    include new router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from .car import router as car_router
-from .exception_nums import router as exception_nums_router
+from .exception_nums import router as exception_nums_router, router_start as start_end_time_router
 from .export_data import router as export_data_router
 from .daily_report import router as daily_report_router
 from .unknown_car import router as unknown_car_router
@@ -12,4 +12,6 @@ router.include_router(unknown_car_router, prefix="/unknown-car", tags=["Unknown 
 router.include_router(exception_nums_router, prefix="/exception-nums", tags=["Exception Numbers"])
 router.include_router(export_data_router, prefix="/export-data", tags=["Export Data"])
 router.include_router(daily_report_router, prefix="/daily-report", tags=["Daily Report"])
+
+router.include_router(start_end_time_router, prefix="/start_end_time", tags=["Start-End Time"])
 
