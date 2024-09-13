1aa4c75a8d2525f5d5a13365e46a62dcaee378d3
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-13 17:56:57 +0500

    create table start_end_time model and pydantic schema

--- app/models/exception_nums.py
+++ app/models/exception_nums.py
@@ -8,3 +8,11 @@ class Number(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     number: Mapped[str] = mapped_column()
+
+
+class StartEndTime(Base):
+    __tablename__ = 'start_end_time'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    start_time: Mapped[str] = mapped_column(default='07:00')
+    end_time: Mapped[str] = mapped_column(default='19:00')
--- app/schemas/exception_nums.py
+++ app/schemas/exception_nums.py
@@ -4,3 +4,9 @@ from pydantic import BaseModel
 class NumbersResponse(BaseModel):
     id: int
     number: str
+
+
+class StartEndTimeResponse(BaseModel):
+    id: int
+    start_time: str = '07:00'
+    end_time: str = '19:00'
