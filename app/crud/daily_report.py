c4ad44c8574a1cfc72eed132be0097369ecab736
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-13 17:37:29 +0500

    correct export data of report, add total stmt

--- app/crud/daily_report.py
+++ app/crud/daily_report.py
@@ -38,11 +38,10 @@ async def get_daily_report(db: AsyncSession, date: str):
 
 async def store_daily_report():
     current_date = datetime.now(current_tz).strftime("%Y-%m-%d")
-    date = '2024-09-10'
 
     async for session in get_async_session():
         async with session.begin():
-            response = await get_cars(db=session, page=1, limit=10, date=date)
+            response = await get_cars(db=session, page=1, limit=10, date=current_date)
 
             top10 = response["top10"]
             total_cars = response["total_cars"]
@@ -63,7 +62,7 @@ async def store_daily_report():
                         cars_attendances_count += car["attend_count"]
 
             daily_report = DailyReport(
-                date=date,
+                date=current_date,
                 top10=top10_cars,
                 general=general_cars,
                 general_attendances_count=cars_attendances_count,
@@ -75,7 +74,7 @@ async def store_daily_report():
 
 
 def schedule_daily_report():
-    schedule_time = datetime.now(current_tz).replace(hour=16, minute=5, second=0, microsecond=0)
+    schedule_time = datetime.now(current_tz).replace(hour=23, minute=30, second=0, microsecond=0)
     schedule.every().day.at(schedule_time.strftime("%H:%M")).do(lambda: asyncio.create_task(store_daily_report()))
 
 
@@ -96,6 +95,10 @@ async def create_excel_report(db: AsyncSession, date: str):
         data = await get_car(db=db, date=date, page=None, limit=None, car_number=None)
         formated_data = [car if car["car_number"] in daily_report_car_numbers else None for car in data]
         formated_response = []
+
+        total_cars = 0
+        total_attendance = 0
+
         for car in formated_data:
             if car is not None:
                 for attend in daily_report.general:
@@ -108,12 +111,31 @@ async def create_excel_report(db: AsyncSession, date: str):
                                 "last_time": car["last_time"],
                             }
                         )
+                        total_cars += 1
+                        total_attendance += attend["attend_count"]
+
+        formated_response.append({"car_number": "", "attend_count": "", "first_time": "", "last_time": ""})
+
+        formated_response.append(
+            {
+                "car_number": f"{total_cars} cars",
+                "attend_count": f"total {total_attendance}",
+                "first_time": "",
+                "last_time": "",
+            }
+        )
 
         excel_report = await create_excel_file(formated_response, file_name=f"daily_report_{date}")
 
         return excel_report
     else:
         formated_general_data = []
+
+        total_daily_count = 0
+        total_paid_car = 0
+        total_non_paid_car = 0
+        total_general_count = 0
+
         for report in daily_report:
             daily_count = 0
             non_paid_count = report.overall_count - report.general_count
@@ -126,8 +148,26 @@ async def create_excel_report(db: AsyncSession, date: str):
                     "average_come": daily_count / report.general_count if report.general_count != 0 else 0,
                     "paid_car": report.general_count,
                     "non_paid_car": non_paid_count,
+                    "general_count": report.general_attendances_count,
                 }
             )
+            total_daily_count += daily_count
+            total_paid_car += report.general_count
+            total_non_paid_car += non_paid_count
+            total_general_count += report.general_attendances_count or 0
+
+        formated_general_data.append({"date": "", "daily_count": "", "average_come": "", "paid_car": "", "non_paid_car": "", "general_count": ""})
+
+        formated_general_data.append(
+            {
+                "date": "Total",
+                "daily_count": total_daily_count,
+                "average_come": total_daily_count / total_paid_car if total_paid_car != 0 else 0,
+                "paid_car": total_paid_car,
+                "non_paid_car": total_non_paid_car,
+                "general_count": total_general_count,
+            }
+        )
 
         excel_report = await create_excel_file(formated_general_data, file_name=f"monthly_report_{date}")
 
