72e6ed85135f2072d5190fb163017e7e3109b459
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-13 09:41:08 +0500

    temporary

--- app/crud/daily_report.py
+++ app/crud/daily_report.py
@@ -37,10 +37,11 @@ async def get_daily_report(db: AsyncSession, date: str):
 
 async def store_daily_report():
     current_date = datetime.now(current_tz).strftime("%Y-%m-%d")
+    date='2024-09-12'
 
     async for session in get_async_session():
         async with session.begin():
-            response = await get_cars(db=session, page=1, limit=10, date=current_date)
+            response = await get_cars(db=session, page=1, limit=10, date=date)
 
             top10 = response["top10"]
             total_cars = response["total_cars"]
@@ -59,7 +60,7 @@ async def store_daily_report():
                         general_cars.append(car)
 
             daily_report = DailyReport(
-                date=current_date,
+                date=date,
                 top10=top10_cars,
                 general=general_cars,
                 general_count=len(general_cars),
@@ -70,7 +71,7 @@ async def store_daily_report():
 
 
 def schedule_daily_report():
-    schedule_time = datetime.now(current_tz).replace(hour=9, minute=40, second=0, microsecond=0)
+    schedule_time = datetime.now(current_tz).replace(hour=9, minute=42, second=0, microsecond=0)
     schedule.every().day.at(schedule_time.strftime("%H:%M")).do(lambda: asyncio.create_task(store_daily_report()))
 
 
