79696b4a5ab95f248e60621c531ce5577671bc37
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-18 13:09:59 +0500

    time sleep

--- app/crud/cars/get_cars_by_month.py
+++ app/crud/cars/get_cars_by_month.py
@@ -49,7 +49,7 @@ async def get_cars_by_month_pag(
 
     calculation_duration = (time.time() - calculation_start) * 1000
 
-    time.sleep(.300)
+    time.sleep(.500)
 
     total_func_duration = (time.time() - total_func_start) * 1000
 
