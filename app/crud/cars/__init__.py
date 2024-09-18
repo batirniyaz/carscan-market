2f2342a5f009d894c1275ef76760b3807ade5019
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-18 15:51:12 +0500

    correct all funcs

--- app/crud/cars/__init__.py
+++ app/crud/cars/__init__.py
@@ -1,5 +1,5 @@
 from .get_cars_by_day import get_cars_by_day
-from .get_cars_by_month import get_cars_by_month, get_cars_by_month_pag
+from .get_cars_by_month import get_cars_by_month
 from .get_cars_by_week import get_cars_by_week
 # from car_processes import (
 #     process_top10_response,
